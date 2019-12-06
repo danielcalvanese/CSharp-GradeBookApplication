daa75f18cb86e91ebb432a26e07720a7c34b3315
Daniel Calvanese <Daniel.Calvanese@Gmail.com>
2019-12-06 12:42:51 -0500

    Updated the RankedGradeBook to calculate letter grades.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,10 +11,36 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            // Refuse to use a ranked grade book for a class of fewer than five students.
+            if (Students.Count < 5) throw new InvalidOperationException();
+
+            // Resolve a count of students in each letter grade section.
+            int letterGradeSectionCount = Students.Count / 5;
+
+            // Resolve averages grades for all students, reversed sorted.
+            List<double> studentsAverageGradesReverseSorted = Students.ConvertAll(s => s.AverageGrade);
+            studentsAverageGradesReverseSorted.Sort();
+            studentsAverageGradesReverseSorted.Reverse();
+
+            // Calculate grade cutoffs.
+            double gradeCutoffA = studentsAverageGradesReverseSorted[letterGradeSectionCount*1 - 1];
+            double gradeCutoffB = studentsAverageGradesReverseSorted[letterGradeSectionCount*2 - 1];
+            double gradeCutoffC = studentsAverageGradesReverseSorted[letterGradeSectionCount*3 - 1];
+            double gradeCutoffD = studentsAverageGradesReverseSorted[letterGradeSectionCount*4 - 1];
+
+            // Return grade letters.
+            if (averageGrade >= gradeCutoffA) return 'A';
+            else if (averageGrade >= gradeCutoffB) return 'B';
+            else if (averageGrade >= gradeCutoffC) return 'C';
+            else if (averageGrade >= gradeCutoffD) return 'D';
+            else return 'F';
         }
     }
 }
